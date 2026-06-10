d0ec533df53f0f2eddbb3d6acf46e29982ad3529
Michael Mbugua <michaelmbugua777@gmail.com>
2026-06-10 13:23:03 +0300

    set up transporter for node mailer

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { PORT } from "./config/env.js";
 import connectDB from "./database/dbconnection.js";
+import transporter from "./config/mailer.js";
 
 const app = express();
 // External Middlewares
@@ -16,3 +17,10 @@ app.listen(PORT, async () => {
   console.log(`Server is running on http://localhost:${PORT}`);
   await connectDB();
 });
+
+try {
+  await transporter.verify();
+  console.log("Server is ready to take our messages");
+} catch (err) {
+  console.error("Verification failed:", err);
+}
