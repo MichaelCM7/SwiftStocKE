6dc10efdef555caceb2d714da4b173511f8599e9
Michael Mbugua <michaelmbugua777@gmail.com>
2026-06-10 16:43:22 +0300

    add header and footer componets boiler plate

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import './Footer.css';
+
+export function Footer() {
+  return (
+    <div className='footer'>
+      <h1>Footer</h1>
+    </div>
+  )
+}
---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import './Header.css';
+
+export function Header() {
+  return (
+    <div className='header'>
+      <h1>Header</h1>
+    </div>
+  )
+}
