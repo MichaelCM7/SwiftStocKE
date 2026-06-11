5b7a34511bd25a3f2f25cab9f2ef5df3571bb4ce
Michael Mbugua <michaelmbugua777@gmail.com>
2026-06-11 12:49:49 +0300

    add sign up basic logic

--- frontend/src/pages/Authentication/SignIn/SignIn.jsx
+++ frontend/src/pages/Authentication/SignIn/SignIn.jsx
@@ -1,6 +1,20 @@
-import './SignIn.css'
+import './SignIn.css';
+import axios from 'axios';
+import { useState } from 'react';
 
 export function SignIn() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    axios.post('/api/auth/sign-in', {
+      email: email,
+      password: password
+    })
+  }
+
   return (
     <>
       <h1>SignIn Page</h1>
