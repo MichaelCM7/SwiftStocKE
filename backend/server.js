adf6fdeedf684e6476c9af6045784cbbc808a610
Michael Mbugua <michaelmbugua777@gmail.com>
2026-06-14 21:57:41 +0300

    add error handler middleware

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,11 @@ import cookieParser from "cookie-parser";
 import { urlencoded } from "express";
 // API Routes
 import authRoutes from "./routes/auth.routes.js";
+// API Middlewares
+import authorize from "./middlewares/authorization.middleware.js";
+import errorHandler from "./middlewares/error.middleware.js";
+
+
 
 const app = express();
 // External Middlewares
@@ -18,14 +23,14 @@ app.use(urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // API Routes
-app.use('/api/auth/', authRoutes);
+app.use('/api/auth/', authorize, authRoutes, errorHandler);
 
 app.listen(PORT, async () => {
   console.log(`Server is running on http://localhost:${PORT}`);
   await connectDB();
 });
 
-console.log(dayjs().format('DD-MM-YYYY HH:mm:ss'));
+// console.log(dayjs().format('DD-MM-YYYY HH:mm:ss'));
 
 // Test Nodemailer transporter (Uncomment When Sending Emails)
 // try {
