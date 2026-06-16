1816c4df2768add07cb29171503135cb349ec876
Michael Mbugua <michaelmbugua777@gmail.com>
2026-06-16 19:31:49 +0300

    create history model

---
+++ backend/models/history.model.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const historySchema = new mongoose.Schema({
+  retailer: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Retailer',
+    required: true,
+    index: true
+  },
+  itemName: {
+    type: String,
+    required: true,
+    minLength: 3,
+    maxLength: 50,
+    trim: true
+  },
+  change:{
+    type: String,
+    required: true,
+    trim: true,
+    minLength: 10,
+    maxlength: 150
+  },
+  dateTime:{
+    type: String,
+    required: true,
+    trim: true,
+    maxLength: 17,
+    minLength: 17
+  }
+}, {timestamps:true});
+
+const History = mongoose.model('History', historySchema);
+
+export default History;
