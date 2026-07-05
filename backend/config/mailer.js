6de9996168d9a0504aa61b7d8f11d6d96c98487b
Michael Mbugua <michaelmbugua777@gmail.com>
2026-07-05 23:56:06 +0300

    fix transporter attmpt 2

--- backend/config/mailer.js
+++ backend/config/mailer.js
@@ -3,8 +3,8 @@ import {SMTP_USER, SMTP_PASS} from '../config/env.js'
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true, // use STARTTLS (upgrade connection to TLS after connecting)
+  port: 587,
+  secure: false, // use STARTTLS (upgrade connection to TLS after connecting)
   // In case of issues with nodemailer uncomment the line below
   // family: 4, 
   auth: {
