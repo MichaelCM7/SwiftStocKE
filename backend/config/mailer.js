9ece34273f4e9a367c53c1ce09c3188e99d20f5d
Michael Mbugua <michaelmbugua777@gmail.com>
2026-07-05 23:49:42 +0300

    use secure mail

--- backend/config/mailer.js
+++ backend/config/mailer.js
@@ -3,14 +3,15 @@ import {SMTP_USER, SMTP_PASS} from '../config/env.js'
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false, // use STARTTLS (upgrade connection to TLS after connecting)
+  port: 465,
+  secure: true, // use STARTTLS (upgrade connection to TLS after connecting)
   // In case of issues with nodemailer uncomment the line below
   // family: 4, 
   auth: {
     user: SMTP_USER,
     pass: SMTP_PASS,
   },
+  connectionTimeout: 10000,
 });
 
 export default transporter;
