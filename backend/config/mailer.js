0ebbf17b2f53c6420592a220f3b7f09d2d72a63b
Michael Mbugua <michaelmbugua777@gmail.com>
2026-07-06 14:16:37 +0300

    uncomment. Trialls unsuccessful

--- backend/config/mailer.js
+++ backend/config/mailer.js
@@ -6,15 +6,15 @@ dns.setDefaultResultOrder('ipv4first');
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587, // 587 // 465
+  port: 587,
   secure: false, // use STARTTLS (upgrade connection to TLS after connecting)
   // In case of issues with nodemailer uncomment the line below
-  family: 4, 
+  // family: 4, 
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
-  connectionTimeout: 30000,
+  // connectionTimeout: 30000,
 });
 
 export default transporter;
