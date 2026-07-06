cc3af8bac0f871181f58b0c62dbcef5dab9c9db1
Michael Mbugua <michaelmbugua777@gmail.com>
2026-07-06 13:43:19 +0300

    attempt 5 of fixing issue with sign uo on deployed evnvironment

--- backend/config/env.js
+++ backend/config/env.js
@@ -2,14 +2,28 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const {
-  PORT,
-  MONGO_URI,
-  SMTP_USER,
-  SMTP_PASS,
-  JWT_SECRET,
-  JWT_EXPIRES_IN,
-  OTP_SECRET,
-  OTP_EXPIRES_IN,
-  EMAIL_TOKEN_SECRET
-} = process.env;
+export const PORT = process.env.PORT;
+export const MONGO_URI = process.env.MONGO_URI;
+
+export const SMTP_USER = process.env.SMTP_USER;
+export const SMTP_PASS = process.env.SMTP_PASS;
+
+export const JWT_SECRET = process.env.JWT_SECRET;
+export const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN;
+
+export const OTP_SECRET = process.env.OTP_SECRET;
+export const OTP_EXPIRES_IN = process.env.OTP_EXPIRES_IN;
+
+export const EMAIL_TOKEN_SECRET = process.env.EMAIL_TOKEN_SECRET;
+
+// export const {
+//   PORT,
+//   MONGO_URI,
+//   SMTP_USER,
+//   SMTP_PASS,
+//   JWT_SECRET,
+//   JWT_EXPIRES_IN,
+//   OTP_SECRET,
+//   OTP_EXPIRES_IN,
+//   EMAIL_TOKEN_SECRET
+// } = process.env;
--- backend/config/mailer.js
+++ backend/config/mailer.js
@@ -1,17 +1,17 @@
 import nodemailer from 'nodemailer';
-import {SMTP_USER, SMTP_PASS} from '../config/env.js'
+// import {SMTP_USER, SMTP_PASS} from '../config/env.js'
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false, // use STARTTLS (upgrade connection to TLS after connecting)
+  port: 465, // 587 // 465
+  secure: true, // use STARTTLS (upgrade connection to TLS after connecting)
   // In case of issues with nodemailer uncomment the line below
   family: 4, 
   auth: {
-    user: SMTP_USER,
-    pass: SMTP_PASS,
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
   },
-  connectionTimeout: 10000,
+  connectionTimeout: 30000,
 });
 
 export default transporter;
